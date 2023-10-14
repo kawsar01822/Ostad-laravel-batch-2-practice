5070c5e10879f4e149aacac512d69145d274efbe
Kawsar Ahmed <kaws.med@gmail.com>
2023-10-15 00:28:00 +0600

    Add files via upload

---
+++ module_5/blood bank/app/Classes/VolunteerDonor.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace app\Classes;
+
+use app\Classes\BloodDonor;
+
+class VolunteerDonor extends BloodDonor
+{
+    public function donateBlood()
+    {
+        echo $this->name . " is Donating Blood\n";
+    }
+}
